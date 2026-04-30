b57309958d5e87fa6880bf2675b6e1c8d6db6cfd
Leonid Gurianov <leonid.11521@gmail.com>
2026-04-30 19:30:11 +0300

    add env

--- src/constants.py
+++ src/constants.py
@@ -1,13 +1,12 @@
 from fastapi.security import OAuth2PasswordBearer
+import os
 
 SCHEME = OAuth2PasswordBearer(tokenUrl="login")
-SECRET_KEY = (
-    "M&6u_hdJ-)Qvx:yGkN+2|1a917e3c181d9399a72327e00f78ff8a9992aeea|3ae318fbdc247ce171ea90c244cc46e4"
-)
+SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 15
 REFRESH_TOKEN_EXPIRE_MINUTES = 10080
 
-DB_URL = "postgresql+asyncpg://postgres:root@postgres-sso-service:5433/postgres"
+DB_URL = os.getenv("DB_URL")
 
 ADMIN_USERNAME = "admin"
